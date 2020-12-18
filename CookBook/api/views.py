76cc264419302de9626f689d683748471632f344
Paweł Boryszko <pawelb95@hotmail.com>
2020-12-18 20:02:43 +0100

    repairing the login and logout

--- CookBook/api/views.py
+++ CookBook/api/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate 
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.shortcuts import render
 from django.http import HttpResponse
 from .serializers import CreateUserSerializer, UserSerializer, UpdateUserPasswordSerializer, UpdateUserSerializer, RecipeSerializer, CreateRecipeSerializer
